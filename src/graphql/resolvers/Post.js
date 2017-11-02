e8620cb5df7c0d06d591b67f960525408d180f93
Julia Randall <jrandall@nutritionix.com>
2017-11-02 17:06:57 -0400

    setting up graphQL

---
+++ src/graphql/resolvers/Post.js
@@ -0,0 +1,32 @@
+'use strict';
+//RESOLVER - Provides a mapping between a portion of a GraphQL operation and actual backend code
+//responsible for handling it(CONTROLLER in MVC)
+const axios = require('axios');
+
+const PostsController = {
+
+	index: ( args ) => {
+
+		const URL = `https://www.reddit.com/r/${ args.subreddit || 'javascript' }.json`;
+
+		return axios.get( URL )
+			.then( (response) => {
+				const __posts = [];
+				const posts = response.data.data.children;
+
+				posts.map( post => {
+					post.data.content = post.data.selftext_html;
+					__posts.push( post.data );
+				} );
+				return __posts;
+			})
+			.catch( (error) => {
+				return { error: error }
+			});
+
+	}
+
+
+}
+
+module.exports = PostsController;
